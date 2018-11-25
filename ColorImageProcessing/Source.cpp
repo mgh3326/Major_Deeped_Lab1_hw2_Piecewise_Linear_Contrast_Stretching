3b920aaf46af781a410989b81fc5a986e9f39227
mgh3326 <mgh3326@naver.com>
2018-11-25 14:23:08 +0900

    function reformat update

--- ColorImageProcessing/Source.cpp
+++ ColorImageProcessing/Source.cpp
@@ -20,124 +20,39 @@ unsigned char** mem_alloc2_d(int n_height, int n_width, unsigned char nlnit_val)
 	return rtn;
 }
 
-unsigned char** padding(unsigned char** in, int n_height, int n_width, int n_filter_size)
-{
-	int n_pad_size = (int)(n_filter_size / 2);
-	unsigned char** pad = mem_alloc2_d(n_height + 2 * n_pad_size, n_width + 2 * n_pad_size, 0);
-
-	for (int h = 0; h < n_height; h++)
-	{
-		for (int w = 0; w < n_width; w++)
-		{
-			pad[h + n_pad_size][w + n_pad_size] = in[h][w];
-		}
-	}
-
-	for (int h = 0; h < n_pad_size; h++)
-	{
-		for (int w = 0; w < n_width; w++)
-		{
-			pad[h][w + n_pad_size] = in[0][w];
-			pad[h + (n_height - 1)][w + n_pad_size] = in[n_height - 1][w];
-		}
-	}
-
-	for (int h = 0; h < n_height; h++)
-	{
-		for (int w = 0; w < n_pad_size; w++)
-		{
-			pad[h + n_pad_size][w] = in[h][0];
-			pad[h + n_pad_size][w + (n_width - 1)] = in[h][n_width - 1];
-		}
-	}
-
-	for (int h = 0; h < n_pad_size; h++)
-	{
-		for (int w = 0; w < n_pad_size; w++)
-		{
-			pad[h][w] = in[0][0];
-			pad[h + (n_height - 1)][w] = in[n_height - 1][0];
-			pad[h][w + (n_width - 1)] = in[0][n_width - 1];
-			pad[h + (n_height - 1)][w + (n_width - 1)] = in[n_height - 1][n_width - 1];
-		}
-	}
-
-	return pad;
-}
 unsigned char LinearContrastStretching(unsigned char input) {
-	//직선 기울기에 따라 바뀌는걸 만들어보자
-// ( 50,10) , (200,210)
-	// (100,10) (150,200)
-	// (50,30) (200, 230)
-// y = (1/5)x
-// y= 4x -190
-// y = (10/21)x
 	unsigned char out;
-	/*if (input < 50.0)
-	{
-		out = (30.0 / 50.0)*input;
-		return out;
-
-	}
-	else if (input < 200.0)
-	{
-		out = ((230.0-30.0)/(200.0-50.0))*(input - 50.0) + 30.0 ;
-		return out;
-
-	}
-	else if(input <256) {
-		out = ((255.0-230.0/255.0-200.0)) * (input- 200.0) + 230.0;
-		return out;
-
-	}*/
 	if (input <= 50) out = round((25.0 / 50.0)*input);//25	50
-	else if (input > 50 && input <= 120) out = round((double)((200.0-25.0) / (120.0-50.0)) * (input - 50.0) + 25.0);//200	120
-	else if (input > 120) out = round((double)((255.0-200.0)/(255-120)) * (input - 120.0) + 200.0);//255	255
+	else if (input > 50 && input <= 120) out = round((double)((200.0 - 25.0) / (120.0 - 50.0)) * (input - 50.0) + 25.0);//200	120
+	else if (input > 120) out = round((double)((255.0 - 200.0) / (255 - 120)) * (input - 120.0) + 200.0);//255	255
 	return out;
 
 }
-int main(void)
+typedef struct _RGB {
+	unsigned char b;//받을때 bgr순서라서 이렇게 함
+	unsigned char g;
+	unsigned char r;
+} RGB;
+int readRGB(unsigned char** r, unsigned char** g, unsigned char** b, int nHeight_in, int nWidth_in, string file_name)
 {
-	unsigned char** in_data = mem_alloc2_d(X_MAX, Y_MAX, 0);
+	RGB** rgb = new RGB*[nHeight_in];
 
-
-	FILE* in = fopen("lena512.rgb", "rb");
+	FILE* in = fopen(file_name.c_str(), "rb");
 	if (in == NULL)
 	{
 		printf("File not found!!\n");
-		return 0;
+		exit(1);
+		return 1;
 	}
 
-
-	/*FILE* in_input = fopen("filter_input.raw", "rb");*/
-
-
-
-	int nHeight_in = Y_MAX;
-	int nWidth_in = X_MAX;
-	unsigned char** ch_in_1 = mem_alloc2_d(nHeight_in, nWidth_in, 0);
-
-	unsigned char** ch_in_2 = mem_alloc2_d(nHeight_in, nWidth_in, 0);
-
-	unsigned char** ch_in_3 = mem_alloc2_d(nHeight_in, nWidth_in, 0);
-
-
-
-	typedef struct {
-		unsigned char r;
-		unsigned char g;
-		unsigned char b;
-	} RGB;
-
-	RGB** rgb = new RGB*[512];
-	for (int n = 0; n < 512; n++)
+	for (int n = 0; n < nHeight_in; n++)
 	{
-		rgb[n] = new RGB[512];
-		memset(rgb[n], 0, sizeof(RGB) * 512);
+		rgb[n] = new RGB[nWidth_in];
+		memset(rgb[n], 0, sizeof(RGB) * nWidth_in);
 	}
-	for (int i = 0; i < Y_MAX; i++)
+	for (int i = 0; i < nHeight_in; i++)
 	{
-		fread(rgb[i], sizeof(RGB), X_MAX, in);
+		fread(rgb[i], sizeof(RGB), nWidth_in, in);
 	}
 	fclose(in);
 	for (int h = 0; h < nHeight_in; h++)
@@ -146,44 +61,15 @@ int main(void)
 		for (int w = 0; w < nWidth_in; w++)
 
 		{
-			ch_in_1[h][w] = rgb[h][w].r;
-			ch_in_2[h][w] = rgb[h][w].g;
-			ch_in_3[h][w] = rgb[h][w].b;
+			r[h][w] = rgb[h][w].r;
+			g[h][w] = rgb[h][w].g;
+			b[h][w] = rgb[h][w].b;
 		}
 	}
-	for (int h = 0; h < nHeight_in; h++)
-	{
-		for (int w = 0; w < nWidth_in; w++)
-
-		{
-			ch_in_1[h][w] = LinearContrastStretching(ch_in_1[h][w]);
-			ch_in_2[h][w] = LinearContrastStretching(ch_in_2[h][w]);
-			ch_in_3[h][w] = LinearContrastStretching(ch_in_3[h][w]);
-		}
-	}
-	//FILE* outfile1 = fopen("input.raw", "w+b");
-	//for (int i = 0; i < Y_MAX; i++)
-	//{
-	//	fwrite(rgb[i], sizeof(RGB), X_MAX, outfile1);
-	//}
-	//직선 기울기에 따라 바뀌는걸 만들어보자
-	// ( 50,10) , (100,210)
-	// y = (1/5)x
-	// y= 4x -190
-	// y = (10/21)x
-	/*for (int h = 0; h < nHeight_in; h++)
-
-	{
-		for (int w = 0; w < nWidth_in; w++)
-
-		{
-		
-			ch_in_1[h][w] = rgb[h][w].r;
-			ch_in_2[h][w] = rgb[h][w].g;
-			ch_in_3[h][w] = rgb[h][w].b;
-		}
-	}*/
-	/*fclose(in_input);*/
+	return 0;
+}
+int writeRGB(unsigned char** r, unsigned char** g, unsigned char** b, int nHeight_in, int nWidth_in, string file_name)
+{
 	RGB** out = new RGB*[512];
 	for (int n = 0; n < 512; n++)
 	{
@@ -196,65 +82,45 @@ int main(void)
 		for (int w = 0; w < nWidth_in; w++)
 
 		{
-			out[h][w].r = ch_in_1[h][w];
-			out[h][w].g = ch_in_2[h][w];
-			out[h][w].b = ch_in_3[h][w];
-			
+			out[h][w].r = r[h][w];
+			out[h][w].g = g[h][w];
+			out[h][w].b = b[h][w];
+
 		}
 	}
-	FILE* outfile = fopen("ouput2.raw", "w+b");
+	FILE* outfile = fopen(file_name.c_str(), "w+b");
 	for (int i = 0; i < Y_MAX; i++)
 	{
 		fwrite(out[i], sizeof(RGB), X_MAX, outfile);
 	}
 
-	//sharpening_filter(in_data_Filter1);
-	//in_input = fopen("lena256_n5.raw", "rb");
-	//unsigned char** in_data_2 = mem_alloc2_d(X_MAX, Y_MAX, 0);
-
-	//if (in_input == NULL)
-	//{
-	//	printf("File not found!!\n");
-	//	return 0;
-	//}
-	//for (int i = 0; i < Y_MAX; i++)
-	//{
-	//	fread(in_data_2[i], sizeof(char), X_MAX, in_input);
-	//}
-	//fclose(in_input);
-	//median_filter(3, in_data_2, "n5");
-	//in_input = fopen("lena256_n10.raw", "rb");
-
-	//unsigned char** in_data_3 = mem_alloc2_d(X_MAX, Y_MAX, 0);
-
-	//if (in_input == NULL)
-	//{
-	//	printf("File not found!!\n");
-	//	return 0;
-	//}
-	//for (int i = 0; i < Y_MAX; i++)
-	//{
-	//	fread(in_data_3[i], sizeof(char), X_MAX, in_input);
-	//}
-	//fclose(in_input);
-	//median_filter(3, in_data_3, "n10");
-	//in_input = fopen("lena256_n25.raw", "rb");
+	return 0;
 
 
-	//unsigned char** in_data_4 = mem_alloc2_d(X_MAX, Y_MAX, 0);
+}
+int main(void)
+{
+	int nHeight_in = Y_MAX;
+	int nWidth_in = X_MAX;
+	unsigned char** ch_in_r = mem_alloc2_d(nHeight_in, nWidth_in, 0);//rgb가 아니라 bgr로 들어오는것 같기도 하다. 한번 알아보고 해야겠다.
+	unsigned char** ch_in_g = mem_alloc2_d(nHeight_in, nWidth_in, 0);
+	unsigned char** ch_in_b = mem_alloc2_d(nHeight_in, nWidth_in, 0);
 
-	//if (in_input == NULL)
-	//{
-	//	printf("File not found!!\n");
-	//	return 0;
-	//}
-	//for (int i = 0; i < Y_MAX; i++)
-	//{
-	//	fread(in_data_4[i], sizeof(char), X_MAX, in_input);
-	//}
-	//fclose(in_input);
-	//median_filter(3, in_data_4, "n25");
-	//
+
+	readRGB(ch_in_r, ch_in_g, ch_in_b, nHeight_in, nWidth_in, "lena512.rgb");
+
+	for (int h = 0; h < nHeight_in; h++)
+	{
+		for (int w = 0; w < nWidth_in; w++)
+
+		{
+			ch_in_r[h][w] = LinearContrastStretching(ch_in_r[h][w]);
+			ch_in_g[h][w] = LinearContrastStretching(ch_in_g[h][w]);
+			ch_in_b[h][w] = LinearContrastStretching(ch_in_b[h][w]);
+		}
+	}
+
+	writeRGB(ch_in_r, ch_in_g, ch_in_b, nHeight_in, nWidth_in, "out.raw");
 
 	return 0;
 }
